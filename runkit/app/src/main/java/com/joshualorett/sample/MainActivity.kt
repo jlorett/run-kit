e4512c0dc17754142fb904fbde537977a7df2bf4
jal <joshlorett@gmail.com>
2020-07-07 19:50:46 -0400

    Update package to include sample

--- runkit/app/src/main/java/com/joshualorett/sample/MainActivity.kt
+++ runkit/app/src/main/java/com/joshualorett/sample/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.joshualorett.runkit
+package com.joshualorett.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.joshualorett.runkit.sample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
