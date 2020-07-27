796428abd8f0e20d568ad59cdf4f91e8c51d07f6
jal <joshlorett@gmail.com>
2020-07-26 20:29:26 -0400

    Add permission request for fine location

--- runkit/app/src/main/java/com/joshualorett/sample/MainActivity.kt
+++ runkit/app/src/main/java/com/joshualorett/sample/MainActivity.kt
@@ -2,11 +2,27 @@ package com.joshualorett.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.joshualorett.runkit.sample.R
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                showMessage("Permission granted.")
+            } else {
+                showMessage("Permission denied.")
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
