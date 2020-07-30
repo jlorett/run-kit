b6769c04a8a58ac03c2b423ddd4276f0a44bc6a8
jal <joshlorett@gmail.com>
2020-07-30 19:28:03 -0400

    Make call to get last known location

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainActivity.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainActivity.kt
@@ -2,11 +2,37 @@ package com.joshualorett.fusedapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                showMessage("Permission granted.")
+                fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                    Log.d("log", "Location: $location")
+                }
+            } else {
+                showMessage("Permission denied.")
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
