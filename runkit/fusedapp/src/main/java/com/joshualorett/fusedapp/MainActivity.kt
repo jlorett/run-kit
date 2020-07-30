0512ca0fef692a1e8ffee1d2f870017ab0feac54
jal <joshlorett@gmail.com>
2020-07-30 19:42:33 -0400

    Move location call into new function requiring permission

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainActivity.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainActivity.kt
@@ -1,22 +1,25 @@
 package com.joshualorett.fusedapp
 
+import android.Manifest
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    @SuppressLint("MissingPermission")
     private val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
             if (isGranted) {
                 showMessage("Permission granted.")
-                fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-                    Log.d("log", "Location: $location")
-                }
+                logLastLocation()
             } else {
                 showMessage("Permission denied.")
             }
@@ -25,14 +28,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
-
-
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
     }
 
     private fun showMessage(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
+    private fun logLastLocation() {
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            Log.d("log", "Location: $location")
+        }
     }
 }
