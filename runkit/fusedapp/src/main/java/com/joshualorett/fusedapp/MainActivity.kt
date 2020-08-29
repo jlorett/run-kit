77b2ead23e904d813c1f6c0b99df4cffb8eed1f5
jal <joshlorett@gmail.com>
2020-08-28 21:38:23 -0400

    Add permission check in case permisson was revoked while service was running

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainActivity.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.joshualorett.fusedapp
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.SharedPreferences
 import android.location.Location
 import android.os.Bundle
 import android.widget.Toast
@@ -11,65 +10,65 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
+class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationListener: FusedLocationObserver
 
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         fusedLocationListener = FusedLocationObserver(this, lifecycle) { locationData ->
             when(locationData) {
                 is LocationData.Success -> updateLocationUi(locationData.location)
-                is LocationData.Error -> showMessage(locationData.exception.toString())
+                is LocationData.Error.PermissionError -> {
+                    setUiState(false)
+                    showMessage(locationData.exception.toString())
+                }
             }
         }
         fusedLocationListener.registerLifecycle(lifecycle)
-
-        val isRequestingUpdates = LocationUpdatePreferences.requestingLocationUpdates(this)
-        if(isRequestingUpdates) {
-            withPermission(Manifest.permission.ACCESS_FINE_LOCATION,
-                run = {
-                },
-                fallback = {
-                    fusedLocationListener.stopUpdates()
-                    showMessage("Location permission missing.")
-                })
-        }
     }
 
     @SuppressLint("MissingPermission")
     override fun onStart() {
         super.onStart()
-        LocationUpdatePreferences.getSharedPreferences(this)
-            .registerOnSharedPreferenceChangeListener(this)
         actionBtn.setOnClickListener {
             val isRequestingUpdates = LocationUpdatePreferences.requestingLocationUpdates(this)
             if(isRequestingUpdates) {
+                setUiState(false)
                 fusedLocationListener.stopUpdates()
             } else {
-               withPermission(
+                withPermission(
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     run = {
                         fusedLocationListener.startUpdates()
+                        setUiState(true)
                     },
                     fallback = {
+                        setUiState(false)
                         showMessage("Location permission missing.")
                     }
                 )
             }
         }
-        setUiState(LocationUpdatePreferences.requestingLocationUpdates(this))
-    }
-
-    override fun onStop() {
-        LocationUpdatePreferences.getSharedPreferences(this)
-            .unregisterOnSharedPreferenceChangeListener(this)
-        super.onStop()
+        checkPermission()
     }
 
     override fun onDestroy() {
         fusedLocationListener.unregisterLifecycle(lifecycle)
         super.onDestroy()
+    }
+
+    private fun checkPermission() {
+        val isRequestingUpdates = LocationUpdatePreferences.requestingLocationUpdates(this)
+        if(isRequestingUpdates && !hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            fusedLocationListener.stopUpdates()
+            LocationUpdatePreferences.setRequestingLocationUpdates(this, false)
+            setUiState(false)
+            showMessage("Location permission missing.")
+        } else {
+            setUiState(LocationUpdatePreferences.requestingLocationUpdates(this))
+        }
     }
 
     private fun showMessage(message: String) {
@@ -89,13 +88,6 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
             location.text = "--"
             time.text = ""
             actionBtn.text = getString(R.string.start)
-        }
-    }
-
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-        // Update the buttons state depending on whether location updates are being requested.
-        if (key == LocationUpdatePreferences.requestLocationUpdatesKey) {
-            setUiState(LocationUpdatePreferences.requestingLocationUpdates(this))
         }
     }
 }
