972fc9ab002061d2a7d33d9345e2d209d093d276
jal <joshlorett@gmail.com>
2020-09-07 14:37:25 -0400

    Create location tracker to convert system location updates to flow

---
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/FusedLocationTracker.kt
@@ -0,0 +1,89 @@
+package com.joshualorett.fusedapp
+
+import android.content.Context
+import android.location.Location
+import android.os.Looper
+import android.util.Log
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.NonCancellable.cancel
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.sendBlocking
+import kotlinx.coroutines.flow.*
+
+/**
+ * Created by Joshua on 9/7/2020.
+ */
+class FusedLocationTracker(context: Context) {
+    // todo move to config
+    private val updateIntervalMs: Long = 10000
+    private val fastestUpdaterIntervalMs = updateIntervalMs / 2
+
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context.applicationContext)
+    private val locationRequest = LocationRequest().apply {
+        interval = updateIntervalMs
+        fastestInterval = fastestUpdaterIntervalMs
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+    private var location: Location? = null
+
+    fun track(): Flow<Location> {
+        return getLocationUpdates()
+            .combine(getLastLocation().filterNotNull()) { lastLocation, location ->
+                if (location.time > lastLocation.time) {
+                    location
+                } else {
+                    lastLocation
+                }
+            }
+            .conflate()
+            .onEach { location -> this.location = location }
+    }
+
+    private fun getLocationUpdates(): Flow<Location> {
+        return callbackFlow {
+            val locationCallback = object: LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult?) {
+                    super.onLocationResult(locationResult)
+                    locationResult?.lastLocation?.let {
+                        sendBlocking(it)
+                    }
+                }
+            }
+            try {
+                fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+            } catch (exception: SecurityException) {
+                cancel("Lost location permission. Could not request updates. $exception")
+            }
+
+            awaitClose { fusedLocationClient.removeLocationUpdates(locationCallback) }
+        }
+    }
+
+    private fun getLastLocation(): Flow<Location?> {
+        return callbackFlow {
+             val onComplete: OnCompleteListener<Location> = OnCompleteListener<Location> { task ->
+                 if(task.isSuccessful && task.result != null) {
+                     sendBlocking(task.result)
+                 } else {
+                     sendBlocking(null)
+                 }
+             }
+            try {
+                fusedLocationClient.lastLocation.addOnCompleteListener(onComplete)
+            } catch (exception: SecurityException) {
+                cancel("Lost location permission. Could not request updates. $exception")
+            }
+
+            awaitClose {
+                cancel()
+            }
+        }
+    }
+}
