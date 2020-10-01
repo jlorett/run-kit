34120c915016a7066fa54e6c6c3cfb0987a44efb
jal <joshlorett@gmail.com>
2020-10-01 19:30:11 -0400

    Update FusedLocationUpdateService to LifecycleService
    This allows us to work with FusedLocationTracker's coroutine based
    implementation easier. A trackLocationJob is used to help us stop and
    start location tracking easily by simply cancelling it.

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/FusedLocationUpdateService.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/FusedLocationUpdateService.kt
@@ -8,17 +8,22 @@ import android.location.Location
 import android.os.*
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.lifecycleScope
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.android.gms.location.LocationServices
 import com.joshualorett.fusedapp.location.FusedLocationTracker
 import com.joshualorett.fusedapp.location.FusedLocationSettings
+import com.joshualorett.fusedapp.session.SessionDao
+import com.joshualorett.fusedapp.session.SessionDataStore
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
 import java.text.DateFormat
 import java.util.*
 
 @ExperimentalCoroutinesApi
-class FusedLocationUpdateService : Service() {
+class FusedLocationUpdateService : LifecycleService() {
     companion object {
         const val pkgName = "com.joshualorett.fusedapp.locationupdatesservice"
         const val actionBroadcast: String = "$pkgName.broadcast"
@@ -28,13 +33,12 @@ class FusedLocationUpdateService : Service() {
         private const val channelId = "channel_fused_location"
         private val tag = FusedLocationUpdateService::class.java.simpleName
     }
-    private val job = SupervisorJob()
-    private val scope = CoroutineScope(Dispatchers.Default + job)
-    private var locationUpdateJob: Job? = null
+    private var trackLocationJob: Job? = null
     private val binder: IBinder = FusedLocationUpdateServiceBinder()
     private lateinit var notificationManager: NotificationManager
     private lateinit var serviceHandler: Handler
     private lateinit var locationTracker: FusedLocationTracker
+    private val sessionDao: SessionDao = SessionDataStore
     /**
      * Used to check whether the bound activity has really gone away and not unbound as part of an
      * orientation change. We create a foreground service notification only if the former takes
@@ -43,6 +47,7 @@ class FusedLocationUpdateService : Service() {
     private var changingConfiguration = false
 
     override fun onCreate() {
+        super.onCreate()
         val handlerThread = HandlerThread(tag)
         handlerThread.start()
         serviceHandler = Handler(handlerThread.looper)
@@ -58,6 +63,7 @@ class FusedLocationUpdateService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
         Log.i(tag, "Service started")
         val startedFromNotification = intent?.getBooleanExtra(extraStartedFromNotification, false) ?: false
         // We got here because the user decided to remove location updates from the notification.
@@ -75,6 +81,7 @@ class FusedLocationUpdateService : Service() {
     }
 
     override fun onBind(intent: Intent): IBinder {
+        super.onBind(intent)
         // Called when a client comes to the foreground and binds with this service. The service
         // should cease to be a foreground service when that happens.
         Log.i(tag, "in onBind()")
@@ -96,45 +103,50 @@ class FusedLocationUpdateService : Service() {
         Log.i(tag, "Last client unbound from service")
         // Called when the last client unbinds from this service. If this method is called due to a
         // configuration change, we do nothing. Otherwise, we make this service a foreground service.
-        if (!changingConfiguration && LocationUpdatePreferences.requestingLocationUpdates(this)) {
-            Log.i(tag, "Starting foreground service")
-            startForeground(notificationId, getNotification())
+        lifecycleScope.launch {
+            val requestingUpdates = sessionDao.getSessionFlow().first()
+            if (!changingConfiguration && requestingUpdates) {
+                Log.i(tag, "Starting foreground service")
+                startForeground(notificationId, getNotification())
+            }
         }
         return true // Ensures onRebind() is called when a client re-binds.
     }
 
     override fun onDestroy() {
         serviceHandler.removeCallbacksAndMessages(null)
-        job.cancel()
+        super.onDestroy()
     }
 
     fun requestLocationUpdates() {
         Log.i(tag, "Requesting location updates")
-        LocationUpdatePreferences.setRequestingLocationUpdates(this, true)
         startService(Intent(applicationContext, FusedLocationUpdateService::class.java))
         try {
-            scope.launch {
-                locationUpdateJob = launch {
-                    locationTracker.track().collect { location ->
-                        onNewLocation(location)
-                    }
+            trackLocationJob = lifecycleScope.launch {
+                sessionDao.setInSession(true)
+                locationTracker.track().collect { location ->
+                    onNewLocation(location)
                 }
             }
         } catch (exception: SecurityException) {
-            LocationUpdatePreferences.setRequestingLocationUpdates(this, false)
             Log.e(tag, "Lost location permission. Could not request updates. $exception")
+            lifecycleScope.launch {
+                sessionDao.setInSession(false)
+            }
         }
     }
 
     fun removeLocationUpdates() {
         Log.i(tag, "Removing location updates")
         try {
-            locationUpdateJob?.cancel()
+            trackLocationJob?.cancel()
         } catch (exception: SecurityException) {
             Log.e(tag, "Lost location permission. Could not remove updates. $exception")
         } finally {
-            LocationUpdatePreferences.setRequestingLocationUpdates(this, false)
-            stopSelf()
+            lifecycleScope.launch {
+                sessionDao.setInSession(false)
+                stopSelf()
+            }
         }
     }
 
@@ -181,7 +193,7 @@ class FusedLocationUpdateService : Service() {
     private fun serviceIsRunningInForeground(context: Context): Boolean {
         val activityManager: ActivityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         for(service: ActivityManager.RunningServiceInfo in activityManager.getRunningServices(Integer.MAX_VALUE)) {
-            if(javaClass.name.equals(service.service.className)) {
+            if(javaClass.name == service.service.className) {
                 if(service.foreground) {
                     return true
                 }
