12ee35b51d2fa38fc99211f43136fe24fbaea5c5
jal <joshlorett@gmail.com>
2020-10-01 19:29:16 -0400

    Add SessionDataStore to access when the app has started and stopped a run session
    This uses a DataStore which allows us to observe when the session is changed

---
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/session/SessionDao.kt
@@ -0,0 +1,12 @@
+package com.joshualorett.fusedapp.session
+
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Data access object for a session.
+ * Created by Joshua on 9/27/2020.
+ */
+interface SessionDao {
+    fun getSessionFlow(): Flow<Boolean>
+    suspend fun setInSession(inSession: Boolean)
+}
---
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/session/SessionDataStore.kt
@@ -0,0 +1,37 @@
+package com.joshualorett.fusedapp.session
+
+import android.content.Context
+import androidx.datastore.DataStore
+import androidx.datastore.preferences.Preferences
+import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.edit
+import androidx.datastore.preferences.preferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+/**
+ * Data storage for Session data.
+ * Created by Joshua on 9/27/2020.
+ */
+object SessionDataStore: SessionDao {
+    private lateinit var dataStore: DataStore<Preferences>
+    private val inSessionKey = preferencesKey<Boolean>("inSession")
+
+    fun init(context: Context) {
+        if (!::dataStore.isInitialized) {
+            dataStore = context.createDataStore(name = "session")
+        }
+    }
+
+    override fun getSessionFlow(): Flow<Boolean> {
+        return dataStore.data.map{ preferences ->
+            preferences[inSessionKey] ?: false
+        }
+    }
+
+    override suspend fun setInSession(inSession: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[inSessionKey] = inSession
+        }
+    }
+}
