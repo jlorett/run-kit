00cdbd6cf3b0075b1bcd174cada4f5779c6d4509
jal <joshlorett@gmail.com>
2020-10-01 19:33:35 -0400

    Add MainViewModel keep track of session state

---
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainViewModel.kt
@@ -0,0 +1,18 @@
+package com.joshualorett.fusedapp
+
+import androidx.lifecycle.*
+import com.joshualorett.fusedapp.session.SessionDao
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.onEach
+
+/**
+ * [ViewModel] for Main view.
+ * Created by Joshua on 9/27/2020.
+ */
+class MainViewModel(sessionDao: SessionDao): ViewModel() {
+    var inSession = false
+    val sessionLiveData: LiveData<Boolean> = sessionDao.getSessionFlow()
+        .conflate()
+        .onEach { inSession = it }
+        .asLiveData()
+}
