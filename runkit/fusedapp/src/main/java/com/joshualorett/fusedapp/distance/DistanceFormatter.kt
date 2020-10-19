f196bb766d2d1e85230abfebe51eb1ae2cf8366a
jal <joshlorett@gmail.com>
2020-10-18 20:22:26 -0400

    Include function to format distance text

---
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/distance/DistanceFormatter.kt
@@ -0,0 +1,15 @@
+package com.joshualorett.fusedapp.distance
+
+/**
+ * Formatted distance text.
+ * Created by Joshua on 10/18/2020.
+ */
+fun formatDistance(distance: Float): String {
+    return if (distance < 1000) {
+        val roundedDistance = "%.2f".format(distance)
+        "$roundedDistance m"
+    } else {
+        val roundedDistance = "%.2f".format(distance/1000)
+        "$roundedDistance km"
+    }
+}
