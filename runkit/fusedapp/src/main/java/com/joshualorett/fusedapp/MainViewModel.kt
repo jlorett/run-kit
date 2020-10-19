82536a8338e9dba51390d31bd4564dfde5faa530
jal <joshlorett@gmail.com>
2020-10-18 20:23:00 -0400

    Add distance flow to push current distance to ui

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainViewModel.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainViewModel.kt
@@ -3,18 +3,27 @@ package com.joshualorett.fusedapp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import com.joshualorett.fusedapp.distance.DistanceDao
+import com.joshualorett.fusedapp.distance.formatDistance
 import com.joshualorett.fusedapp.session.SessionDao
 import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 
 /**
  * [ViewModel] for Main view.
  * Created by Joshua on 9/27/2020.
  */
-class MainViewModel(sessionDao: SessionDao): ViewModel() {
+class MainViewModel(sessionDao: SessionDao, distanceDao: DistanceDao): ViewModel() {
     var inSession = false
     val sessionLiveData: LiveData<Boolean> = sessionDao.getSessionFlow()
         .conflate()
         .onEach { inSession = it }
+        .asLiveData()
+    val distanceLiveData: LiveData<String> = distanceDao.getDistanceFlow()
+        .map { distance ->
+            formatDistance(distance)
+        }
+        .conflate()
         .asLiveData()
 }
