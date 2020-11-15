0992d1ef08a7263385ee23286552d9b68a55cb40
jal <joshlorett@gmail.com>
2020-11-15 16:59:15 -0500

    Add SessionService interface to interact with a location tracking session

---
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/session/SessionService.kt
@@ -0,0 +1,15 @@
+package com.joshualorett.fusedapp.session
+
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Interact with a location tracking session.
+ * Created by Joshua on 11/14/2020.
+ */
+interface SessionService {
+    val sessionFlow: Flow<Session>
+    fun start()
+    fun stop()
+    fun trackingLocationFlow(): Boolean
+    suspend fun inSession(): Boolean
+}
