48204f4ed3af63d17c3ba46b5529e676bc47aa26
jal <joshlorett@gmail.com>
2020-11-15 17:03:55 -0500

    Migrate FusedLocationObserver to MainActivity
    Separating the observer felt unnecessary as it was mostly just binding to the service.

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainActivity.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainActivity.kt
@@ -2,7 +2,12 @@ package com.joshualorett.fusedapp
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
@@ -14,6 +19,30 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
+    private var bound = false
+
+    private val fusedServiceConnection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val binder: FusedSessionService.FusedLocationUpdateServiceBinder = service as FusedSessionService.FusedLocationUpdateServiceBinder
+            viewModel = MainViewModel(binder.service)
+            viewModel.observeSession().observe(this@MainActivity, { session ->
+                val state = session.state
+                if (state == Session.State.IDLE) {
+                    setUiState(false)
+                } else if (state == Session.State.STARTED) {
+                    setTime(session.time)
+                    setUiState(session.state == Session.State.STARTED)
+                    setDistance(formatDistance(session.distance))
+                }
+            })
+            viewModel.checkSession(hasPermission(Manifest.permission.ACCESS_FINE_LOCATION))
+            bound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            bound = false
+        }
+    }
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,24 +51,14 @@ class MainActivity : AppCompatActivity() {
         if (!SessionDataStore.initialized) {
             SessionDataStore.init(applicationContext)
         }
-        val fusedLocationObserver = FusedLocationObserver(this, lifecycle)
-        viewModel = MainViewModel(fusedLocationObserver)
-        viewModel.sessionLiveData.observe(this, { session ->
-            val state = session.state
-            if (state == Session.State.IDLE) {
-                setUiState(false)
-                //setDistance("--")
-            } else if (state == Session.State.STARTED) {
-                setTime(session.time)
-                setUiState(session.state == Session.State.STARTED)
-                setDistance(formatDistance(session.distance))
-            }
-        })
     }
 
     @SuppressLint("MissingPermission")
     override fun onStart() {
         super.onStart()
+        // Bind to the service. If the service is in foreground mode, this signals to the service
+        // that since this activity is in the foreground, the service can exit foreground mode.
+        bindService(Intent(this, FusedSessionService::class.java), fusedServiceConnection, Context.BIND_AUTO_CREATE)
         actionBtn.setOnClickListener {
             val inSession = viewModel.inSession
             if (inSession) {
@@ -56,13 +75,16 @@ class MainActivity : AppCompatActivity() {
                 )
             }
         }
-        checkPermission()
     }
 
-    private fun checkPermission() {
-        if(viewModel.inSession && !hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            viewModel.stop()
-            showMessage("Location permission missing.")
+    override fun onStop() {
+        super.onStop()
+        if (bound) {
+            // Unbind from the service. This signals to the service that this activity is no longer
+            // in the foreground, and the service can respond by promoting itself to a foreground
+            // service.
+            unbindService(fusedServiceConnection)
+            bound = false
         }
     }
 
--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainViewModel.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/MainViewModel.kt
@@ -3,28 +3,53 @@ package com.joshualorett.fusedapp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.joshualorett.fusedapp.session.Session
+import com.joshualorett.fusedapp.session.SessionService
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 /**
  * [ViewModel] for Main view.
  * Created by Joshua on 9/27/2020.
  */
-class MainViewModel(private val fusedLocationObserver: FusedLocationObserver): ViewModel() {
+class MainViewModel(private val fusedLocationUpdateService: SessionService): ViewModel() {
     var inSession = false
-    val sessionLiveData: LiveData<Session> = fusedLocationObserver.sessionFlow
-        .onEach { inSession = it.state == Session.State.STARTED }
-        .asLiveData()
 
     fun start() {
         try {
-            fusedLocationObserver.startUpdates()
+            fusedLocationUpdateService.start()
         } catch (e: SecurityException) {
             stop()
         }
     }
 
     fun stop() {
-        fusedLocationObserver.stopUpdates()
+        fusedLocationUpdateService.stop()
+    }
+
+    fun observeSession(): LiveData<Session> {
+        return fusedLocationUpdateService.sessionFlow
+            .onEach { inSession = it.state == Session.State.STARTED }
+            .asLiveData()
+    }
+
+    fun checkSession(hasPermission: Boolean) {
+        viewModelScope.launch {
+            val inSession = fusedLocationUpdateService.inSession()
+            val trackingLocation = fusedLocationUpdateService.trackingLocationFlow()
+            //Tracking stopped, restarting location tracking.
+            if (inSession && hasPermission && !trackingLocation) {
+                try {
+                    start()
+                } catch (e: SecurityException) {
+                    stop()
+                }
+            }
+            //Permission lost, stopping location tracking.
+            if (!hasPermission && inSession) {
+                stop()
+            }
+        }
     }
 }
