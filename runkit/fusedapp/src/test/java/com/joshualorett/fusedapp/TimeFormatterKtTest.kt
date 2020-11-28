96a31f64d4f63cb0370540de8779a3430deda944
jal <joshlorett@gmail.com>
2020-11-28 18:06:28 -0500

    Add formatHourMinuteSeconds for time display

---
+++ runkit/fusedapp/src/test/java/com/joshualorett/fusedapp/TimeFormatterKtTest.kt
@@ -0,0 +1,52 @@
+package com.joshualorett.fusedapp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Tests [formatHourMinuteSeconds].
+ * Created by Joshua on 11/28/2020.
+ */
+class TimeFormatterKtTest {
+
+    @Test
+    fun lessThanASecond() {
+        assertEquals("00:00:00", formatHourMinuteSeconds(100))
+    }
+
+    @Test
+    fun justSeconds() {
+        assertEquals("00:00:01", formatHourMinuteSeconds(1000))
+    }
+
+    @Test
+    fun justMinutes() {
+        assertEquals("00:01:00", formatHourMinuteSeconds(1000*60))
+    }
+
+    @Test
+    fun justHours() {
+        assertEquals("01:00:00", formatHourMinuteSeconds(1000*60*60))
+    }
+
+    @Test
+    fun hourGreaterThanTwoDigits(){
+        assertEquals("100:00:00", formatHourMinuteSeconds(100000*60*60))
+    }
+
+    @Test
+    fun timeCarriesToMinutes() {
+        assertEquals("00:01:30", formatHourMinuteSeconds(90*1000))
+    }
+
+    @Test
+    fun timeCarriesToHours() {
+        assertEquals("01:30:00", formatHourMinuteSeconds(90*60*1000))
+    }
+
+    @Test
+    fun timeCarriesToHoursAndMinutes() {
+        assertEquals("01:30:03", formatHourMinuteSeconds(90*60*1000 + 3*1000))
+    }
+}
