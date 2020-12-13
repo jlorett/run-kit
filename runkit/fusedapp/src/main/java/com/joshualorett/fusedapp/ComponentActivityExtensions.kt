d8358fe9a0a3de3eddf5eff173d23f360ef2f572
jal <joshlorett@gmail.com>
2020-12-13 18:31:03 -0500

    Remove withPermission extension
    Actions should be registered before activity is started, it didn't make sense to use it this way anymore. See startSession in MainActivity for new way.

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/ComponentActivityExtensions.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/ComponentActivityExtensions.kt
@@ -10,19 +10,6 @@ import androidx.activity.result.contract.ActivityResultContracts
  * Created by Joshua on 8/15/2020.
  */
 
-/**
- * Runs a task if permission is granted otherwise, fallback if permission is not granted.
- */
-fun ComponentActivity.withPermission(permission: String, run: () -> Unit, fallback: () -> Unit) {
-    registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-        if (isGranted) {
-            run()
-        } else {
-            fallback()
-        }
-    }.launch(permission)
-}
-
 fun ComponentActivity.hasPermission(permission: String): Boolean {
     return this.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
 }
