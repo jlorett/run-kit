69dbf684a1dbed38b6ecae7accc4b3f82f0321ad
jal <joshlorett@gmail.com>
2021-01-02 18:04:05 -0500

    Add one-to-many entity for session with all its locations

---
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/database/SessionWithLocations.kt
@@ -0,0 +1,17 @@
+package com.joshualorett.fusedapp.database
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+/**
+ * A session with its list of locations.
+ * Created by Joshua on 1/2/2021.
+ */
+data class SessionWithLocations(
+    @Embedded val session: SessionEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "sessionId"
+    )
+    val locations: List<LocationEntity>,
+)
