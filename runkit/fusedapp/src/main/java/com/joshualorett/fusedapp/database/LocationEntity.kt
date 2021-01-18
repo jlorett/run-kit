10b774e185e7619bc8d3fed8ad10fe2cb8cb2e39
jal <joshlorett@gmail.com>
2021-01-17 23:40:20 -0500

    Add entity annotation to LocationEntity

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/database/LocationEntity.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/database/LocationEntity.kt
@@ -1,12 +1,13 @@
 package com.joshualorett.fusedapp.database
 
-import android.location.Location
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 /**
  * Room entity for a location.
  * Created by Joshua on 1/2/2021.
  */
+@Entity
 data class LocationEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
