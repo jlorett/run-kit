baae8501c15622d46d083f8f43b943f218efba86
jal <joshlorett@gmail.com>
2021-01-22 23:41:16 -0500

    Update room dao with session state and current session calls

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/database/RoomSessionDao.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/database/RoomSessionDao.kt
@@ -1,6 +1,7 @@
 package com.joshualorett.fusedapp.database
 
 import androidx.room.*
+import com.joshualorett.fusedapp.session.Session
 import kotlinx.coroutines.flow.Flow
 
 /**
@@ -16,6 +17,10 @@ interface RoomSessionDao {
     fun getSessionDistance(id: Long): Flow<Float>
     @Query("SELECT elapsedTime FROM SessionEntity WHERE id=:id")
     fun getSessionElapsedTime(id: Long): Flow<Long>
+    @Query("SELECT state FROM SessionEntity WHERE id=:id")
+    fun getSessionState(id: Long): Flow<Session.State>
+    @Query("SELECT * FROM SessionEntity WHERE state=1 OR state=2 ORDER BY date DESC LIMIT 1")
+    fun getCurrentSession(): Flow<SessionEntity?>
     @Insert
     fun createSession(session: SessionEntity): Long
     @Insert
@@ -24,4 +29,6 @@ interface RoomSessionDao {
     fun updateSessionDistance(id: Long, distance: Float)
     @Query("UPDATE SessionEntity SET elapsedTime=:elapsedTime WHERE id=:id")
     fun updateSessionElapsedTime(id: Long, elapsedTime: Long)
+    @Query("UPDATE SessionEntity SET state=:sessionState WHERE id=:id")
+    fun updateSessionState(id: Long, sessionState: Session.State)
 }
