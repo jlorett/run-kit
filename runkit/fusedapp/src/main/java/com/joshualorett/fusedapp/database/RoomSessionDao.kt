9b1fa7f322f7d0bac0c7032c6b920e89f5306231
jal <joshlorett@gmail.com>
2021-01-22 20:57:31 -0500

    Add additional options for roomdao

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/database/RoomSessionDao.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/database/RoomSessionDao.kt
@@ -1,9 +1,7 @@
 package com.joshualorett.fusedapp.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Transaction
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Data Access Object for Room Session Database.
@@ -13,9 +11,17 @@ import androidx.room.Transaction
 interface RoomSessionDao {
     @Transaction
     @Query("SELECT * FROM SessionEntity")
-    fun getSessionWithLocations(): List<SessionWithLocations>
+    fun getSessionWithLocations(): Flow<List<SessionWithLocations>>
+    @Query("SELECT distance FROM SessionEntity WHERE id=:id")
+    fun getSessionDistance(id: Long): Flow<Float>
+    @Query("SELECT elapsedTime FROM SessionEntity WHERE id=:id")
+    fun getSessionElapsedTime(id: Long): Flow<Long>
     @Insert
     fun createSession(session: SessionEntity): Long
     @Insert
     fun addLocation(location: LocationEntity): Long
+    @Query("UPDATE SessionEntity SET distance=:distance WHERE id=:id")
+    fun updateSessionDistance(id: Long, distance: Float)
+    @Query("UPDATE SessionEntity SET elapsedTime=:elapsedTime WHERE id=:id")
+    fun updateSessionElapsedTime(id: Long, elapsedTime: Long)
 }
