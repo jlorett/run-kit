9ed0dabf8df105cc8e16c17eb920c275d6109f44
jal <joshlorett@gmail.com>
2021-02-06 00:14:30 -0500

    Add extension to convert SessionEntity to Session

---
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/database/SessionEntryExtensions.kt
@@ -0,0 +1,12 @@
+package com.joshualorett.fusedapp.database
+
+import com.joshualorett.fusedapp.session.Session
+
+/**
+ * Extensions for [SessionEntity].
+ * Created by Joshua on 1/31/2021.
+ */
+
+fun SessionEntity.toSession(): Session {
+    return Session(id, title, date, elapsedTime, distance, state)
+}
