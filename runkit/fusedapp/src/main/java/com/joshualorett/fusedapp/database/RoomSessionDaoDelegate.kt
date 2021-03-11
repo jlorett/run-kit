97c5b942fd919cb68a55f1734f0c38ca8163b394
jal <joshlorett@gmail.com>
2021-03-10 20:15:50 -0500

    Update SessionDao to take session id when updating data

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/database/RoomSessionDaoDelegate.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/database/RoomSessionDaoDelegate.kt
@@ -1,13 +1,11 @@
 package com.joshualorett.fusedapp.database
 
 import android.location.Location
-import com.joshualorett.fusedapp.database.active.RoomActiveSessionDao
 import com.joshualorett.fusedapp.database.location.LocationEntity
 import com.joshualorett.fusedapp.database.location.RoomLocationDao
 import com.joshualorett.fusedapp.session.Session
 import com.joshualorett.fusedapp.session.SessionDao
 import com.joshualorett.fusedapp.toIsoString
-import kotlinx.coroutines.flow.*
 import java.util.*
 
 /**
@@ -16,72 +14,48 @@ import java.util.*
  */
 object RoomSessionDaoDelegate: SessionDao {
     private lateinit var sessionDao: RoomSessionDao
-    private lateinit var activeSessionDao: RoomActiveSessionDao
     private lateinit var locationDao: RoomLocationDao
     var initialized = false
 
     fun init(roomSessionDao: RoomSessionDao,
-             activeSessionDao: RoomActiveSessionDao,
             locationDao: RoomLocationDao) {
         if (!initialized) {
             this.sessionDao = roomSessionDao
-            this.activeSessionDao = activeSessionDao
             this.locationDao = locationDao
             initialized = true
         }
     }
 
-    override fun getActiveSessionFlow(): Flow<Session> {
-        return activeSessionDao.getActiveSession().map { sessionEntity ->
-                sessionEntity?.toSession() ?: Session()
-            }
-    }
-
-    override suspend fun setSessionState(sessionState: Session.State) {
-        var sessionId = getCurrentSessionId()
+    override suspend fun setSessionState(id: Long, sessionState: Session.State) {
         when(sessionState) {
             Session.State.STARTED -> {
-                if(sessionId == 0L) {
-                    sessionId = createSession()
-                }
+                val sessionId = if(id == 0L) createSession() else id
                 sessionDao.updateSessionState(sessionId, Session.State.STARTED)
             }
             Session.State.STOPPED -> {
-                sessionDao.updateSessionState(sessionId, Session.State.STOPPED)
+                sessionDao.updateSessionState(id, Session.State.STOPPED)
             }
             Session.State.PAUSED -> {
-                sessionDao.updateSessionState(sessionId, Session.State.PAUSED)
+                sessionDao.updateSessionState(id, Session.State.PAUSED)
             }
         }
     }
 
-    override suspend fun setElapsedTime(time: Long) {
-        val sessionId = getCurrentSessionId()
-        if(sessionId > 0) {
-            sessionDao.updateSessionElapsedTime(sessionId, time)
+    override suspend fun setElapsedTime(id: Long, time: Long) {
+        if(id > 0) {
+            sessionDao.updateSessionElapsedTime(id, time)
         }
     }
 
-    override suspend fun setDistance(distance: Float) {
-        val sessionId = getCurrentSessionId()
-        if(sessionId > 0) {
-            sessionDao.updateSessionDistance(sessionId, distance)
+    override suspend fun setDistance(id: Long, distance: Float) {
+        if(id > 0) {
+            sessionDao.updateSessionDistance(id, distance)
         }
     }
 
-    override suspend fun addLocation(location: Location) {
-        val sessionId = getCurrentSessionId()
-        val locationEntity = toLocationEntity(sessionId, location)
+    override suspend fun addLocation(id: Long, location: Location) {
+        val locationEntity = toLocationEntity(id, location)
         locationDao.addLocation(locationEntity)
-    }
-
-    override suspend fun getSessionLocations(): List<String> {
-        val sessionWithLocations = sessionDao.getSessionWithLocations().first()
-        return sessionWithLocations.map { entity -> entity.toString() }
-    }
-
-    private suspend fun getCurrentSessionId(): Long {
-        return activeSessionDao.getActiveSession().first()?.id ?: 0
     }
 
     private suspend fun createSession(title: String? = null): Long {
--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/session/SessionDao.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/session/SessionDao.kt
@@ -1,17 +1,14 @@
 package com.joshualorett.fusedapp.session
 
 import android.location.Location
-import kotlinx.coroutines.flow.Flow
 
 /**
  * Data access object for a session.
  * Created by Joshua on 9/27/2020.
  */
 interface SessionDao {
-    fun getActiveSessionFlow(): Flow<Session>
-    suspend fun setSessionState(sessionState: Session.State)
-    suspend fun setElapsedTime(time: Long)
-    suspend fun setDistance(distance: Float)
-    suspend fun addLocation(location: Location)
-    suspend fun getSessionLocations(): List<String>
+    suspend fun setSessionState(id: Long, sessionState: Session.State)
+    suspend fun setElapsedTime(id: Long, time: Long)
+    suspend fun setDistance(id: Long, distance: Float)
+    suspend fun addLocation(id: Long, location: Location)
 }
