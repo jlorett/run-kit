3403e15e3ed56b22d820c0933a054454112f0d81
jal <joshlorett@gmail.com>
2021-03-21 00:25:49 -0400

    Add MET values for running to help calculate calories burned

---
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/math/calories/Met.kt
@@ -0,0 +1,22 @@
+package com.joshualorett.fusedapp.math.calories
+
+/**
+ * Metabolic equivalent of task (MET) for an activity.
+ * See [The Compendium of Physical Activities](https://sites.google.com/site/compendiumofphysicalactivities/home)
+ * for more information on how METs are calculated.
+ * Created by Joshua on 3/20/2021.
+ */
+
+const val metFourMph = 6.0
+const val metFiveMph = 8.3
+const val metSixMph = 9.8
+const val metSevenMph = 11.0
+const val metEightMph = 11.8
+const val metNineMph = 12.8
+const val metTenMph = 14.5
+const val metElevenMph = 16.0
+const val metTwelveMph = 19.0
+const val metThirteenMph = 19.8
+const val metFourteenMph = 23.0
+const val metMarathon = 13.3
+const val metRunning = 8.0
