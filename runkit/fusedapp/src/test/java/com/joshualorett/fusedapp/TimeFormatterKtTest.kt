84c22e878a36a958ee021ddd025827bede422df0
jal <joshlorett@gmail.com>
2021-03-25 20:20:32 -0400

    Update formatHoursMinutesSeconds test

--- runkit/fusedapp/src/test/java/com/joshualorett/fusedapp/TimeFormatterKtTest.kt
+++ runkit/fusedapp/src/test/java/com/joshualorett/fusedapp/TimeFormatterKtTest.kt
@@ -1,53 +1,53 @@
 package com.joshualorett.fusedapp
 
-import com.joshualorett.fusedapp.time.formatHourMinuteSeconds
+import com.joshualorett.fusedapp.time.formatHoursMinutesSeconds
 import org.junit.Test
 
 import org.junit.Assert.*
 
 /**
- * Tests [formatHourMinuteSeconds].
+ * Tests [formatHoursMinutesSeconds].
  * Created by Joshua on 11/28/2020.
  */
 class TimeFormatterKtTest {
 
     @Test
     fun lessThanASecond() {
-        assertEquals("00:00:00", formatHourMinuteSeconds(100))
+        assertEquals("00:00:00", formatHoursMinutesSeconds(100.0))
     }
 
     @Test
     fun justSeconds() {
-        assertEquals("00:00:01", formatHourMinuteSeconds(1000))
+        assertEquals("00:00:01", formatHoursMinutesSeconds(1000.0))
     }
 
     @Test
     fun justMinutes() {
-        assertEquals("00:01:00", formatHourMinuteSeconds(1000*60))
+        assertEquals("00:01:00", formatHoursMinutesSeconds(1000.0*60))
     }
 
     @Test
     fun justHours() {
-        assertEquals("01:00:00", formatHourMinuteSeconds(1000*60*60))
+        assertEquals("01:00:00", formatHoursMinutesSeconds(1000.0*60*60))
     }
 
     @Test
     fun hourGreaterThanTwoDigits(){
-        assertEquals("100:00:00", formatHourMinuteSeconds(100000*60*60))
+        assertEquals("100:00:00", formatHoursMinutesSeconds(100000.0*60*60))
     }
 
     @Test
     fun timeCarriesToMinutes() {
-        assertEquals("00:01:30", formatHourMinuteSeconds(90*1000))
+        assertEquals("00:01:30", formatHoursMinutesSeconds(90.0*1000))
     }
 
     @Test
     fun timeCarriesToHours() {
-        assertEquals("01:30:00", formatHourMinuteSeconds(90*60*1000))
+        assertEquals("01:30:00", formatHoursMinutesSeconds(90.0*60*1000))
     }
 
     @Test
     fun timeCarriesToHoursAndMinutes() {
-        assertEquals("01:30:03", formatHourMinuteSeconds(90*60*1000 + 3*1000))
+        assertEquals("01:30:03", formatHoursMinutesSeconds(90.0*60*1000 + 3*1000))
     }
 }
