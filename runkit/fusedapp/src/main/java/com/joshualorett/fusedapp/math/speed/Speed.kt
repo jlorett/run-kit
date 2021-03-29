be4242a70b18756f163a3fce3a7982ac26d11f4b
jal <joshlorett@gmail.com>
2021-03-29 16:35:06 -0400

    Add miles per hour calculation

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/math/speed/Speed.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/math/speed/Speed.kt
@@ -15,3 +15,14 @@ fun kilometersPerHour(meters: Double, milliseconds: Long): Double {
     val kilometers = meters/oneKm
     return kilometers/hours
 }
+
+fun milesPerHour(meters: Double, milliseconds: Long): Double {
+    if(milliseconds == 0L) {
+        return 0.0
+    }
+    val oneMile = 1609.344
+    val oneHour = 3600000.0
+    val hours = milliseconds/oneHour
+    val miles = meters/oneMile
+    return miles/hours
+}
