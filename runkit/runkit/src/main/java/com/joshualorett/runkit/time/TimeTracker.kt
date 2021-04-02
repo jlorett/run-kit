ade9a00f770dc005b13688048bb398c0466561d1
jal <joshlorett@gmail.com>
2021-04-01 21:29:10 -0400

    Replace TimeTracker interface with ElapsedTimeTracker implementation

--- runkit/runkit/src/main/java/com/joshualorett/runkit/time/TimeTracker.kt
+++ runkit/runkit/src/main/java/com/joshualorett/runkit/time/TimeTracker.kt
@@ -1,13 +1,48 @@
 package com.joshualorett.runkit.time
 
+
+import android.os.SystemClock
+
 /**
  * Track time.
  * Created by Joshua on 12/13/2020.
  */
-interface TimeTracker {
-    fun start()
-    fun start(elapsedTime: Long)
-    fun stop()
-    fun reset()
-    fun getElapsedTime(): Long
+class TimeTracker {
+    private var startedTime: Long = 0L
+    private var elapsedTime: Long = 0L
+    var stopped = true
+        private set
+
+    fun start() {
+        stopped = false
+        startedTime = SystemClock.uptimeMillis()
+    }
+
+    fun start(elapsedTime: Long) {
+        this.elapsedTime = elapsedTime
+        stopped = false
+        startedTime = SystemClock.uptimeMillis()
+    }
+
+    fun stop() {
+        stopped = true
+        if(startedTime == 0L) {
+            startedTime = SystemClock.uptimeMillis()
+        }
+        elapsedTime += SystemClock.uptimeMillis() - startedTime
+    }
+
+    fun reset() {
+        stopped = true
+        elapsedTime = 0L
+    }
+
+    fun getElapsedTime(): Long {
+        val now = SystemClock.uptimeMillis()
+        return if (stopped) {
+            elapsedTime
+        } else {
+            elapsedTime + now - startedTime
+        }
+    }
 }
