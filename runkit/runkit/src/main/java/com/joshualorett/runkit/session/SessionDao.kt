0b9b7670ae0cbd98bfab1ae94b669fb7b82791e6
jal <joshlorett@gmail.com>
2021-04-02 19:11:20 -0400

    Add session dao to lib module

---
+++ runkit/runkit/src/main/java/com/joshualorett/runkit/session/SessionDao.kt
@@ -0,0 +1,16 @@
+package com.joshualorett.runkit.session
+
+import android.location.Location
+
+/**
+ * Data access object for a session.
+ * Created by Joshua on 9/27/2020.
+ */
+interface SessionDao {
+    suspend fun createSession(): Long
+    suspend fun setSessionState(id: Long, sessionState: Session.State)
+    suspend fun setElapsedTime(id: Long, time: Long)
+    suspend fun setDistance(id: Long, distance: Float)
+    suspend fun setEndTime(id: Long, endTime: String)
+    suspend fun addLocation(id: Long, location: Location)
+}
