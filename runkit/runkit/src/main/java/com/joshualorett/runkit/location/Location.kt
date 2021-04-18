2f13451ad95bf35a1b33d06408231098316858fd
jal <joshlorett@gmail.com>
2021-04-17 23:56:56 -0400

    Add location class

---
+++ runkit/runkit/src/main/java/com/joshualorett/runkit/location/Location.kt
@@ -0,0 +1,15 @@
+package com.joshualorett.runkit.location
+
+import com.joshualorett.runkit.math.distance.meters
+
+/**
+ * A geographic location.
+ * Created by Joshua on 4/14/2021.
+ */
+data class Location(val latitude: Double,
+                    val longitude: Double,
+                    val timeMs: Long) {
+    fun distanceTo(destination: Location): Double{
+        return meters(latitude, longitude, destination.latitude, destination.longitude)
+    }
+}
