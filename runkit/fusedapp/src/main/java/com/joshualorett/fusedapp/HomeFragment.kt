5f9a4363897b1f53761936f7bdb4168e5b6c30a9
jal <joshlorett@gmail.com>
2021-05-16 00:49:02 -0400

    Add HomeFragment

---
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/HomeFragment.kt
@@ -0,0 +1,40 @@
+package com.joshualorett.fusedapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    private lateinit var startSessionBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        startSessionBtn = view.findViewById(R.id.startSessionBtn)
+        startSessionBtn.setOnClickListener { startNewSession() }
+    }
+
+    private fun startNewSession() {
+        val navController = findNavController()
+        navController.navigate(R.id.action_homeFragment_to_activeSessionFragment)
+    }
+}
