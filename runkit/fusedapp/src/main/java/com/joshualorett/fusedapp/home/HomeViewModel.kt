e3d26d09d47bed69ad4502305734a25625f2350a
jal <joshlorett@gmail.com>
2021-05-22 19:18:28 -0400

    Update list item view

--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/home/HomeViewModel.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.joshualorett.fusedapp.home
 
+import android.os.Build
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
@@ -10,7 +11,12 @@ import androidx.paging.map
 import com.joshualorett.fusedapp.database.RoomSessionDao
 import com.joshualorett.fusedapp.database.SessionEntity
 import com.joshualorett.fusedapp.database.toSession
+import com.joshualorett.runkit.session.Session
 import kotlinx.coroutines.flow.map
+import java.sql.Date
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class HomeViewModel(private val sessionDao: RoomSessionDao): ViewModel() {
     val sessions = Pager(PagingConfig(pageSize = 4)) {
@@ -19,7 +25,39 @@ class HomeViewModel(private val sessionDao: RoomSessionDao): ViewModel() {
         .map {
             it.map {
                     sessionEntity: SessionEntity ->
-                sessionEntity.toSession()
+                val session = sessionEntity.toSession()
+                var title = ""
+                if(session.title == null) {
+                    val startHour = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        LocalDateTime.parse(session.startTime, DateTimeFormatter.ISO_DATE_TIME).hour
+                    } else {
+                        Date.valueOf(session.startTime).hours
+                    }
+                    title = when (startHour) {
+                        in 0..11 -> "Morning Run"
+                        in 12..15 -> "Afternoon Run"
+                        in 16..20 -> "Evening Run"
+                        in 21..23 -> "Night Run"
+                        else -> "Session Run"
+                    }
+                }
+                val endTime = session.endTime?.let {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        LocalDateTime.parse(session.endTime, DateTimeFormatter.ISO_DATE_TIME)
+                            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                    } else {
+                        val dateTimeFormatter = SimpleDateFormat("yyyy-MM-dd")
+                        dateTimeFormatter.format(Date.valueOf(session.endTime))
+                    }
+                } ?: session.endTime
+                Session(
+                    session.id,
+                    title,
+                    session.startTime,
+                    endTime,
+                    session.elapsedTime,
+                    session.distance
+                )
             }
         }
         .cachedIn(viewModelScope)
--- runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/home/SessionViewHolder.kt
+++ runkit/fusedapp/src/main/java/com/joshualorett/fusedapp/home/SessionViewHolder.kt
@@ -4,16 +4,25 @@ import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.joshualorett.fusedapp.R
+import com.joshualorett.fusedapp.formatDistance
+import com.joshualorett.fusedapp.time.formatHoursMinutesSeconds
+import com.joshualorett.fusedapp.time.formatMinutesSeconds
 import com.joshualorett.runkit.session.Session
 
 class SessionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private var date: TextView = itemView.findViewById(R.id.date)
     private var title: TextView = itemView.findViewById(R.id.title)
+    private var distance: TextView = itemView.findViewById(R.id.distance)
+    private var time: TextView = itemView.findViewById(R.id.time)
+    private var pace: TextView = itemView.findViewById(R.id.pace)
 
     fun bind(item: Session?) {
         item?.let {
             date.text = it.endTime
             title.text = it.title
+            distance.text = formatDistance(it.distance)
+            time.text = formatHoursMinutesSeconds(it.elapsedTime.toDouble())
+            pace.text =  "${formatMinutesSeconds(it.averagePace())} /km"
         }
     }
 }
